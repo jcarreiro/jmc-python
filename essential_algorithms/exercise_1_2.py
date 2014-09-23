ac066e4846abb0b40d974a6c81b1365d64dec8ef
Jason Carreiro <jcarreiro@fb.com>
2014-09-23 11:41:50 -0400

    [ea] 1.2: plot the growth of the problem size we can solve

--- essential_algorithms/exercise_1_2.py
+++ essential_algorithms/exercise_1_2.py
@@ -6,6 +6,28 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Orders of growth. For some reason, matplotlib is happy when these are defined
+# as functions, but not happy if I try to give it lambdas?
+def inv_log(s):
+    return 2 ** s
+
+def inv_sqrt(s):
+    return s ** 2
+
+def inv_linear(s):
+    return s
+
+def inv_quad(s):
+    return math.sqrt(s)
+
+def inv_exp(s):
+    return math.log(s, 2)
+
+def inv_fact(s):
+    n = 1
+    while s > math.factorial(n):
+        n += 1
+    return n
 
 def print_table():
     # number of algorithm 'steps' per second
@@ -43,6 +65,24 @@ def print_table():
             except OverflowError:
                 print '  <overflow>',
         print
+
+# Show the growth in the problem size, for each function, as a function of time
+# in seconds.
+def plot_results():
+    t = np.arange(1.0, 100.0) # 100 s
+    fns = [
+        ['log n',  inv_log],
+        ['sqrt n', inv_sqrt],
+        ['n',      inv_linear],
+        ['n^2',    inv_quad],
+        ['2^n',    inv_exp],
+        ['n!',     inv_fact],
+    ]
+    for label, fn in fns:
+        plt.plot(t, [fn(x) for x in t], label=label)
+    plt.yscale('log')
+    plt.legend(loc='upper left')
+    plt.show()
 
 if __name__ == '__main__':
     print_table()
